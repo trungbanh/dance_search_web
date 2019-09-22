7e0256aec9ed3673cf10d7b72bbb8bb7ce2d6a96
trungbanh <trungbanh97@gmail.com>
2019-09-22 16:26:54 +0700

    add post layout

--- query_dance/views.py
+++ query_dance/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from inspect import getmembers
 from pprint import pprint
+from django.views.decorators.csrf import csrf_exempt
+
 import json
 
 
 def index(request):
     return render(request, 'query_dance/index.html')
 
+@csrf_exempt 
 def query(request):
+    print (request.method)
+    if (request.method == 'POST'):
+        print(request.POST)
     return render(request, 'query_dance/query.html')
